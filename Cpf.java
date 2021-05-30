efc96fe94305bb36a6f67d8e89f3700cabb738b1
indigrinada <queirozingrid.c@gmail.com>
2021-05-30 13:33:55 -0300

    alteração de tipo de entrada p/ cpf (de int para String), funcional para números que começam com 0

--- Cpf.java
+++ Cpf.java
@@ -1,56 +1,58 @@
 import java.util.*;
-import java.util.InputMismatchException;
+import java.lang.NumberFormatException;
 
 public class Cpf {
     public int verificarDigito(){
         Scanner input = new Scanner(System.in);
         int i=1;
-        int cont = 0;
-        //boolean validadeCpf=false;
-        //do{
             try {
-                //validadeCpf=true;
                 System.out.println("Digite os primeiros 9 números do seu CPF para obter os dígitos verificadores.\nOBS: DIGITE APENAS OS NÚMEROS, SEM ESPAÇOS NEM SÍMBOLOS\n");
-    
-                int cpf = input.nextInt(); // ler o cpf
-                double cpfDouble = cpf; // a atividade pede que o usuário entre com um inteiro
-                                        //porém, pra trabalhar com resto, precisa ser double
-                                        //então, a conversão
+                String cpfEmTexto = input.nextLine(); // ler o cpf
+                int cpf = Integer.parseInt(cpfEmTexto);
                 
-                System.out.println("*************************************");
-                while (cpf!=0){ // contar quantos dígitos tem o cpf
-                    cpf = cpf/10;
-                    cont++;
-                }
-                if(cont!=9){
-                    System.out.println("O CPF deve conter 9 números. Nada mais, nada menos. Tente novamente.");
-                    //validadeCpf = false;
-                    //   menu.escolher();
+                double cpfDouble = cpf; // para trabalhar com resto, precisa ser double
+                
+                int quantidadeDigitos = cpfEmTexto.length();
+                if(quantidadeDigitos>9 || quantidadeDigitos<8){ // verifica se a quantidade de dígitos é diferente de 9
+                    System.out.println("O CPF deve conter 9 números. Nada mais, nada menos. Tente novamente.\n");
+                    try {
+                        Thread.sleep(3000);
+                        Cpf cpf2 = new Cpf();
+                        cpf2.verificarDigito();
+                        
+                    } catch (Exception erro) {
+                        //
+                    }
+
                 }
                 else{
-                    //validadeCpf = true;
                     // Número para fins de testes: 398136146
                     // Div1 precisa ser 6 e Div2 precisa ser 8
-                    double acumuladoraDiv1 = 0;
-                    double acumuladoraDiv2 = 0;
+                    // Div = dígito verificador
+
+                    double acumuladoraDiv1 = 0; // acumuladora(guardar a soma) dígito verificador 1
+                    double acumuladoraDiv2 = 0; // mesma coisa, só que para o dígito verificador 2
                     double numControle = 0;
-                    while (i<10){
-                        cpfDouble = (int)cpfDouble;
+                    for (i=1; i <= quantidadeDigitos; i++){
+                        cpfDouble = (int)cpfDouble; // o excesso de casas decimais estava fazendo o cálculo dar errado, portanto,
+                                                    // a cada início do loop, ele converte para inteiro
                         acumuladoraDiv1 = ((cpfDouble%10)*(i+1)) + acumuladoraDiv1;
                         acumuladoraDiv2 = ((cpfDouble%10)*(i+2)) + acumuladoraDiv2;
-                        //System.out.println("atualização 2: " + acumuladoraDiv2 + "\n");
-                        // ao tirar o resto da divisão, obtém-se o último dígito do CPF
-                        // multiplica-se então por números a partir de 2 e 3
-                        // (como i=1, acrescentei +1 e +2, para funcionar)
-                        // o resultado, soma-se com o que "já tem"
-                        // na variável acumuladora (que vai acumular)
+                        // --- abaixo, a explicação das duas linhas de código acima!! ---
+                        // o cálculo do resto da divisão de cpfDouble por 10 deve-se ao fato
+                        // de que este valor será o último número do cpf.
+                        // por ex:
+                        // o resto da divisão de 398136146 por 10, será 6
+                        // -----------------------------------------------------------------
+                        // a multiplicação por i+1 ou i+2 é porque os pesos variam entre 2 e 10,
+                        // sendo que o i começa com 1, então acrescentamos +1 na primeira soma e
+                        // +2 na segunda soma
+                        // depois soma-se com o que já tem, por isso, o nome é acumuladora
                         cpfDouble = cpfDouble/10;
-                        //cpf = cpf/10;
                         // a divisão inteira do cpf por 10 consiste em
                         // diminuir um dígito no final
                         // do número todo. Por ex: se era 1234,
                         // agora vai ser 123 e asim por diante
-                        i++;
                     }
                     double div1 = ((acumuladoraDiv1*10) %11);
                     double div2 = ((((acumuladoraDiv2) + div1*2) * 10) % 11);
@@ -64,35 +66,30 @@ public class Cpf {
                     }
     
                     numControle = ((div1*10) + div2);
-    
+                    // abaixo, coloquei pra mostrar os dígitos para fins de confirmacão se o
+                    // cálculo estava funcionando. Está!
                     System.out.println("dig 1: " + div1);
                     System.out.println("dig 2: " + div2);
                     System.out.println("Número de controle do CPF: " + numControle);
                 }
     
-            } catch (InputMismatchException e) {
-                System.out.println("DEVE CONTER APENAS NÚMEROS, TENTE NOVAMENTE!!!");
+            } catch (NumberFormatException e) {
+                System.out.println("DEVE CONTER APENAS NÚMEROS, TENTE NOVAMENTE!!!\n");
                  
                     try {
                         Thread.sleep(3000);
                         Cpf cpf2 = new Cpf();
                         cpf2.verificarDigito();
-                        //Menu menu = new Menu();
-                        //menu.escolher();
-                        //menu.escolher();
+                        
                     } catch (Exception erro) {
                         //
                     }
-                //validadeCpf = false;
-                //validadeCpf = true; 
-                // menu.escolher();
+                    // volta para que o usuário entre novamente com o número do CPF
             }
-
-       // } while(validadeCpf==false);
-        Menu menu = new Menu();
-        menu.escolher();
-       
-        input.close();
-        return 1;
+            Menu menu = new Menu();
+            menu.escolher();
+        
+            input.close();
+            return 1;
     }
 }
--- Menu.java
+++ Menu.java
@@ -10,10 +10,31 @@ public class Menu {
         System.out.println("[2]     Informações do desenvolvedor");
         System.out.println("[3]     SAIR");
 
+        // recebe a opcao que o usuário escolher
         opcao = input.nextInt();
+        // verificar dígito do cpf
         if(opcao==1){
             Cpf cpf = new Cpf();
             cpf.verificarDigito();
+        }
+        // informações sobre mim!!!
+        if(opcao==2){
+            System.out.println("********************* INFORMAÇÕES DO DESENVOLVEDOR *********************\n");
+            System.out.println("    Aluna: Ingrid Queiroz Caetano       Matrícula: 1-2021119626");
+            Menu menu = new Menu();
+            menu.escolher();
+        }
+        // sair fora
+        if(opcao==3){
+            System.exit(0);
+        }
+        // se a pessoa digitar qualquer coisa diferente de 1, 2 ou 3, o menu será exibido novamente
+        // para que ela possa refazer sua escolha sem que o programa desligue sozinho
+        else{
+            System.out.println("********************* !!! ERRO !!! *********************\n");
+            System.out.println("            OPÇÃO INVÁLIDA, TENTE NOVAMENTE!!!");
+            Menu menu = new Menu();
+            menu.escolher();
         }
         input.close();        
     }
